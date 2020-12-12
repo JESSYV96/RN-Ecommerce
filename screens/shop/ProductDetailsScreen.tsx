7bd6174536aa9465eabe5b567d334bd903eb3ada
jessyv96 <c.j91350@gmail.com>
2020-12-12 14:05:27 +0100

    Working on the Product Details Screen

--- screens/shop/ProductDetailsScreen.tsx
+++ screens/shop/ProductDetailsScreen.tsx
@@ -1,21 +1,46 @@
 import React from 'react'
-import { StyleSheet, Text, View , ScrollView} from 'react-native'
+import {
+    StyleSheet,
+    Text,
+    ScrollView,
+    Image,
+    Button,
+    Dimensions,
+    Platform,
+    View
+} from 'react-native'
 import { Props } from '../../types/Props.d'
 import { RootStateOrAny, useSelector } from 'react-redux'
 import Product from '../../models/Product'
+import { Colors } from '../../constants/Colors'
 
 const ProductDetailsScreen = ({ navigation }: Props) => {
     const productId: string = navigation.getParam('id')
     const productTitle: string = navigation.getParam('title')
-     const productList: Product[] = useSelector((state: RootStateOrAny) => state.productList.availableProduct)
+    const productList: Product[] = useSelector((state: RootStateOrAny) => state.productList.availableProduct)
+    const product: Product | undefined = productList.find((p: Product) => p.id === productId)
+
     return (
         <ScrollView>
-            <Text>{productTitle}</Text>
+            <Image
+                style={styles.image}
+                source={{ uri: product?.imageUrl }} />
+            <View style={styles.action}>
+              <Button
+                color={Platform.OS === 'android'
+                    ? Colors.primaryLight
+                    : Colors.primary}
+                onPress={() => { }}
+                title="Ajouter au panier" />  
+            </View>
+            
+            <Text style={styles.price}>{product?.price} €</Text>
+            <Text style={styles.description}>{product?.description}</Text>
         </ScrollView>
     )
 }
 
-ProductDetailsScreen.navigationOptions = (navData:any)  => {
+ProductDetailsScreen.navigationOptions = (navData: any) => {
     return {
         title: navData.navigation.getParam('title')
     }
@@ -23,4 +48,26 @@ ProductDetailsScreen.navigationOptions = (navData:any)  => {
 
 export default ProductDetailsScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: Dimensions.get('window').height / 3
+    },
+    action: {
+        marginVertical: 10,
+        alignItems: 'center'
+    },
+    price: {
+        fontSize: 20,
+        color: '#888',
+        textAlign: 'center',
+        marginVertical: 15
+
+    },
+    description: {
+        fontSize: 15,
+        textAlign: 'center',
+        marginHorizontal: 17
+
+    }
+})
